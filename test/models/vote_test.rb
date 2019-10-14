9dfab0b423e011b9ee16cb0e2606576a82308517
beccaelenzil <beccaelenzil@gmail.com>
2019-10-14 11:11:02 -0700

    votes uniqueness validation and test

--- test/models/vote_test.rb
+++ test/models/vote_test.rb
@@ -10,6 +10,14 @@ describe Vote do
       publication_year: 2019,
       category: "book"
     )
+
+    @work2 = Work.new(
+      title: "Hello World! How are you?",
+      creator: "Becca",
+      description: "blah blah blah",
+      publication_year: 2019,
+      category: "movie"
+    )
   end
   describe "relations" do
     it "can set the work through 'work' " do
@@ -46,6 +54,30 @@ describe Vote do
       vote.user_id = @user.id
 
       expect(vote.user).must_equal @user
+    end
+
+    describe "validations" do
+      it "a user cannot vote on a work twice" do
+        vote = Vote.new(user_id: @user.id, work_id: @work.id)
+        vote.save
+        vote2 = Vote.new(user_id: @user.id, work_id: @work.id)
+        
+        result = vote2.valid?
+
+        expect(result).must_equal false
+      end
+
+      it "a user can vote on 2 different works" do
+        vote = Vote.new(user_id: @user.id, work_id: @work.id)
+        vote.save
+        vote2 = Vote.new(user_id: @user.id, work_id: @work2.id)
+
+        result = vote2.valid?
+
+        expect(result).must_equal true
+        
+      end
+
     end
 
   end
